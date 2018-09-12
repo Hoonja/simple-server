1bda3c1042d55fad8f9cc60a12c83484abd5743e
CHOIJAE HOON <hoonja@CHOIJAEui-iMac.local>
2018-09-12 20:35:31 +0900

    전투 프로토콜 구현

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ var Cmd = {
   ROOM: 'ROOM',
   ROOM_NEWUSER: 'ROOM_NEWUSER',
   ROOM_EXITUSER: 'ROOM_EXITUSER',
+  CONQUER_CELL: 'CONQUER_CELL'
 };
 
 var Res = {
   ROOM_INFO: 'ROOM_INFO',
+  CONQUER_CELL_SUCCESS: 'CONQUER_CELL_SUCCESS',
+  CONQUER_CELL_FAILED: 'CONQUER_CELL_FAILED',
+  UPDATE_CELL: 'UPDATE_CELL',
   LOG: 'LOG'
 };
+
+var BLOCK_INTERVAL = 5000;
 
 var users = [];
 var rooms = [];
@@ -47,7 +53,7 @@ rooms = [];
 qRequest = [[], []];
 curQIndex = 0;
 
-// setInterval(processBlock, 15000);
+setInterval(processBlock, BLOCK_INTERVAL);
 
 http.listen(3000, function () {
   console.log('listening on *:3000');
@@ -56,10 +62,12 @@ http.listen(3000, function () {
 ////////////////////////////////////////////////////////////////////////////////
 //  Operational Functions
 ////////////////////////////////////////////////////////////////////////////////
-function handleChat(socket, data) {  
+function handleChat(socket, data) {
+  console.log('handleChat: ' + JSON.stringify(data));
   if (data.roomId) {
     socket.to(data.roomId).emit(Type.CHAT, {
       userId: data.userId,
+      roomId: data.roomId,
       text: data.text
     });
   } else {  //  서버 전체 유저들을 대상으로 채팅 알림
@@ -71,26 +79,27 @@ function handleChat(socket, data) {
 }
 
 function handleMsg(socket, msg) {
-  // console.log('MSG : ' + JSON.stringify(msg));
   switch (msg.cmd) {
     case Cmd.ROOM:
-      handleMsgOfRoom(socket, msg.data);
+      processRoomMsg(socket, msg.data);
+      break;
+    case Cmd.CONQUER_CELL:
+      addMsgQueue(socket, msg);
       break;
     default:
+      // console.warn('handleMsg: the msg was unhandled [msg.cmd: ' + msg.cmd + ']: ' + JSON.stringify(msg));
       console.warn('handleMsg: the msg was unhandled [msg.cmd: ' + msg.cmd + ']');
       break;
   }  
 }
 
-function handleMsgOfRoom(socket, data) {
+function processRoomMsg(socket, data) {
   socket.join(data.room.id, () => {
     registerUser(socket, data.user, data.room.id);
 
     console.log(data.user.id + '님이 ' + data.room.id + '번 방에 입장했습니다.(socketId: ' + socket.id + ')');
-    //  TODO  유저, 룸 데이터 구조 형성 및 업데이트
     var room = getRoomInfo(data.room.id, data.room.width, data.room.height);
     enterRoom(room, data.user.id);
-
 
     //  해당 방의 모든 멤버에게 입장을 알림
     //  입장한 사람에게 보냄 : 잘 들어왔음을 확인시키기 위함
@@ -99,15 +108,19 @@ function handleMsgOfRoom(socket, data) {
       cmd: Cmd.ROOM_NEWUSER,
       data: data
     });
-    //  TODO  입장한 사람한테는 현재 방의 상황도 알려줘야 함(수정이 필요)
+    
     socket.emit(Type.MSG, {
       cmd: Res.ROOM_INFO,
-      data: {
-        user: data.user,
-        room: room
-      }
+      data: { room: room }
     });
   });
+}
+
+function addMsgQueue(socket, msg) {
+  console.log('큐 인덱스 ' + curQIndex + '에 메세지 추가');
+  qRequest[curQIndex].push({ socket: socket, msg: msg });
+  //  TODO dummy
+  qRequest[curQIndex].push({ socket: socket, msg: {cmd: 'dummy'} });
 }
 
 function getRoomInfo(roomId, width, height) {
@@ -120,7 +133,7 @@ function getRoomInfo(roomId, width, height) {
   function initCells(width, height) {
     var cells = [];
     for (var i = 0; i < width * height; i++) {
-      cells.push({});
+      cells.push({ occupied: false });
     }
     return cells;
   }
@@ -182,7 +195,86 @@ function removeUser(socket) {
   return user;
 }
 
-//  TODO  processBlock
 function processBlock() {
-  console.log('블록 정보가 갱신됩니다.');
+  var prevQIndex = curQIndex;
+  curQIndex = curQIndex === 0 ? 1 : 0;
+  console.log('블록 정보가 갱신됩니다.[처리할 큐인덱스: ' + prevQIndex + ', Length: ' + qRequest[prevQIndex].length + ']');
+  
+  var qConquer = qRequest[prevQIndex].filter(function(item) {
+    return item.msg.cmd === Cmd.CONQUER_CELL;
+  });
+  qConquer.sort(function(a, b) {
+    if (a.msg.roomId === b.msg.roomId) {
+      if (a.msg.data.id === b.msg.data.id) {
+        return b.msg.data.cost - a.msg.data.cost;
+      } else {
+        return a.msg.data.id - b.msg.data.id;
+      }
+    } else {
+      if (a.msg.roomId > b.msg.roomId) {
+        return 1;
+      } else if (a.msg.roomId === b.msg.roomId) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }    
+  });
+  //  정렬 잘 되었나 확인하기 위한 테스트 로그
+  // for (var i = 0; i < qConquer.length; i++) {
+  //   console.log('Sorted result: ' + JSON.stringify(qConquer[i].msg));
+  // }
+  // console.log('Length of CONQUER msg: ' + qConquer.length);
+
+  //  TODO  공격비용이 높은 것에 대해 처리하고..
+  var cellIndex = -1;
+  var roomId = '---';
+  var msg;
+  var socket;
+  var cell;
+  var res;
+  var room;
+  for (var i = 0; i < qConquer.length; i++){
+    msg = qConquer[i].msg;
+    socket = qConquer[i].socket;
+    cell = msg.data;
+    
+    if (roomId !== msg.roomId) {
+      roomId = msg.roomId;
+      cellIndex = -1;
+    }
+
+    room = rooms.find(function(item) {
+      return item.id === roomId;
+    });
+    // console.log('Found room:' + JSON.stringify(room));
+
+    if (room) {
+      if (cellIndex !== cell.id) { //  정렬된 배열중 각 cell의 첫 요소가 공격에 성공한 요청
+        cellIndex = cell.id;
+        room.cells[cell.id] = {
+          id: cell.id,
+          ownerId: msg.userId,
+          team: cell.team,
+          cost: cell.cost,
+          combatCount: room.cells[cell.id].combatCount ? room.cells[cell.id].combatCount + 1 : 1,
+          occupied: true
+        };
+        console.log('전체 방에 셀 정보 업데이트: ' + msg.roomId);
+        socket.to(msg.roomId).emit(Type.MSG, {
+          cmd: Res.UPDATE_CELL,
+          data: room.cells[cell.id]
+        });
+        res = Res.CONQUER_CELL_SUCCESS;
+      } else if (cellIndex === cell.id) { //  나머지는 모두 실패 처리
+        res = Res.CONQUER_CELL_FAILED;
+      }
+      socket.emit(Type.MSG, {
+        cmd: res,
+        data: room.cells[cell.id]
+      });      
+    }    
+  }
+
+  qRequest[prevQIndex] = [];
 }
