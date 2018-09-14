7cdc17686a70804bfdf6a6ec4a973eee76825f68
hoonja <hoonja@gmail.com>
2018-09-14 12:05:51 +0900

    todolist was added

---
+++ todolist.js
@@ -0,0 +1,14 @@
+//  TODO  방 누적 금액 표시하기
+//  TODO  게임이 종료된 판은 더이상 처리되지 않도록
+//  TODO  셀에 마우스 올리면 효과
+//  TODO  게임 종료후 레포팅 기능 강화
+//  TODO  셀 모양 다듬기
+//  TODO  게임 로그 창에 스크롤 기능 탑재
+//  TODO  한 사람이 여러방에 들어갈 수 있게
+//  TODO  현재 열려 있는 방들 정보 조회 후 입장할 수 있게
+//  TODO  소스 코드 구조화 (서버)
+//  TODO  셀을 모드 잃으면 다시 firstAttacked를 false 처리
+//  TODO  스피너 필요한 부분 삽입
+//  TODO  채팅창 UI 구성
+//  TODO  게임판의 셀을 6각으로 만들기
+//  TODO  패킷 형태 통일: { cmd, userId, roomid, data: {subdata}}
